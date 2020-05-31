4c8f59497df3527d6b3edc8a3b86b4920665ce34
Leonardo Nakao <=>
2020-05-31 17:35:23 -0300

    Change default colors theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,18 @@ import Vuetify from "vuetify/lib";
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        dark: true,
+        themes: {
+            dark: {
+                primary: "#9c27b0",
+                secondary: "#673ab7",
+                accent: "#ff9800",
+                error: "#e91e63",
+                warning: "#ff5722",
+                info: "#00bcd4",
+                success: "#4caf50"
+            }
+        }
+    },
 });
